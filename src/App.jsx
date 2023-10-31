2366c74623dbcb95ed5eb896bf9b266f3bd4c36e
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-10-31 18:55:43 -0300

    Add game components to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import './App.css'
+import ChoiceButton from './components/buttons/choiceButton';
+import ResultsScreen from './components/result-screen/resultsScreen';
+import RestartButton from './components/buttons/restartButton';
 
 function App() {
   return (
-    <div className='game'>
-      <div className='header' role='header'>
-        <h2>Rock Paper Scissors!</h2>
-        <p>Choose one of the options:</p>
+    <div className="container">
+      <div className='game'>
+        <div className='header' role='header'>
+          <h2>Rock Paper Scissors!</h2>
+          <p>Choose one of the options:</p>
+        </div>
+        <div className='buttons'>
+          <ChoiceButton type="rock" src="../src/assets/rock-btn.png" hover="../src/assets/rock-btn2.png"/>
+          <ChoiceButton type="paper" src="../src/assets/paper-btn.png" hover="../src/assets/paper-btn2.png"/>
+          <ChoiceButton type="scissors" src="../src/assets/scissors-btn.png" hover="../src/assets/scissors-btn2.png"/>
+        </div>
+        <div className='screen'>
+          <ResultsScreen />
+        </div>
+        <div className='restart'>
+          <RestartButton />
+        </div>
       </div>
-
       <div>
         <img className='background' alt='A very cool rocket!' src='/src/assets/rocket-back2-2.png'></img>
       </div>
