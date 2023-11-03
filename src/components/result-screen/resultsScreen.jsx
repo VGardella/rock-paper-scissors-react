2a6407ef306677d129317d208953b36144799d30
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-11-03 18:27:28 -0300

    Add result message and tests

--- src/components/result-screen/resultsScreen.jsx
+++ src/components/result-screen/resultsScreen.jsx
@@ -1,10 +1,36 @@
 import './resultsScreen.css';
+import PropTypes from 'prop-types';
+import { useState, useEffect } from 'react';
 
-function ResultsScreen() {
+ResultsScreen.propTypes = {
+    computerChoice: PropTypes.string,
+    playerChoice: PropTypes.string,
+    winner: PropTypes.string,
+    playerWins: PropTypes.number,
+    computerWins: PropTypes.number
+}
+
+function ResultsScreen({ computerChoice, playerChoice, winner, playerWins, computerWins }) {
+    const [ roundMessage, setRoundMessage ] = useState(null);
+    const [ counterMessage, setCounterMessage ] = useState(null);
+
+    useEffect(() => {
+        if (winner === null) {
+            setRoundMessage(`You and the computer choose ${computerChoice}; Is's a tie!`);
+            setCounterMessage(`Player: ${playerWins} / Computer: ${computerWins}`);
+        } else if (winner === 'player') {
+            setRoundMessage(`Your choice: ${playerChoice}; Computer's choice: ${computerChoice}. You won!`);
+            setCounterMessage(`Player: ${playerWins} / Computer: ${computerWins}`);
+        } else if (winner === 'computer') {
+            setRoundMessage(`Your choice: ${playerChoice}; Computer's choice: ${computerChoice}. You lost!`);
+            setCounterMessage(`Player: ${playerWins} / Computer: ${computerWins}`);
+        }
+    }, [winner, playerChoice, computerChoice, playerWins, computerWins])
+
     return(
-        <div className='results' role='complementary'>
-            <div id='round-info'>Choose rock, paper or scissors!</div>
-            <div id='counter'></div>
+        <div className='results' role='complementary' >
+            <div data-testid='round-info'>{winner === 0 ? 'Choose rock, paper or scissors!' : roundMessage}</div>
+            <div data-testid='counter'>{winner === 0 ? '' : counterMessage}</div>
         </div>
     )
 }
--- tests/Screen.test.jsx
+++ tests/Screen.test.jsx
@@ -10,3 +10,26 @@ describe('Results screen', () => {
       expect(screen.getByText(/choose/i)).toBeInTheDocument();
     })
   })
+
+describe('Results screen', () => {
+  it('shows correct message: winner', () => {
+    render(<ResultsScreen playerChoice='rock' computerChoice='scissors' winner='player' playerWins={1} computerWins={0} />)
+
+    expect(screen.getByTestId('round-info').textContent).toMatch('Your choice: rock; Computer\'s choice: scissors. You won!')
+    expect(screen.getByTestId('counter').textContent).toMatch('Player: 1 / Computer: 0')
+  })
+
+  it('shows correct message: loser', () => {
+    render(<ResultsScreen playerChoice='rock' computerChoice='paper' winner='computer' playerWins={4} computerWins={3} />)
+
+    expect(screen.getByTestId('round-info').textContent).toMatch('Your choice: rock; Computer\'s choice: paper. You lost!')
+    expect(screen.getByTestId('counter').textContent).toMatch('Player: 4 / Computer: 3')
+  })
+
+  it('shows correct message: tie', () => {
+    render(<ResultsScreen playerChoice='rock' computerChoice='rock' winner={null} playerWins={2} computerWins={2} />)
+
+    expect(screen.getByTestId('round-info').textContent).toMatch('You and the computer choose rock; Is\'s a tie!')
+    expect(screen.getByTestId('counter').textContent).toMatch('Player: 2 / Computer: 2')
+  })
+})
