f20fc5ae3e2fc3b0a8f01d931b5a954f2bb28b47
Victoria Gardella <v.gardella.ruiz@gmail.com>
2023-11-10 18:13:32 -0300

    extract style and unify message conditional render

--- src/components/result-screen/resultsScreen.jsx
+++ src/components/result-screen/resultsScreen.jsx
@@ -7,33 +7,13 @@ ResultsScreen.propTypes = {
     playerChoice: PropTypes.string,
     winner: PropTypes.string,
     playerWins: PropTypes.number,
-    computerWins: PropTypes.number
+    computerWins: PropTypes.number,
+    className: PropTypes.object
 }
 
-function ResultsScreen({ computerChoice, playerChoice, winner, playerWins, computerWins }) {
+function ResultsScreen({ computerChoice, playerChoice, winner, playerWins, computerWins, className }) {
     const [ roundMessage, setRoundMessage ] = useState(null);
     const [ counterMessage, setCounterMessage ] = useState(null);
-    const [ style, setStyle ] = useState(false);
-    let roundStyle, counterStyle, className;
-
-    if (style) {
-        roundStyle = {
-            'fontSize': '30px',
-        };
-        counterStyle = {
-            'display': 'none'
-        }
-        className = 'blink'
-    }
-    else if (!style) {
-        roundStyle = {
-            'fontSize': '20px'
-        };
-        counterStyle = {
-            'display': 'block'
-        }
-        className = ''
-    }
 
     useEffect(() => {
         if (winner === 'tie') {
@@ -46,25 +26,22 @@ function ResultsScreen({ computerChoice, playerChoice, winner, playerWins, compu
             setRoundMessage(`Your choice: ${playerChoice}; Computer's choice: ${computerChoice}. You lost!`);
             setCounterMessage(`Player: ${playerWins} / Computer: ${computerWins}`);
         }
-    }, [winner, playerChoice, computerChoice, playerWins, computerWins])
 
-    useEffect(() => {
         if (playerWins === 5) {
-            setStyle(true)
             setRoundMessage('Player wins!');
             setCounterMessage('');
-          } 
-          if (computerWins === 5) {
-            setStyle(true)
+        }
+
+        if (computerWins === 5) {
             setRoundMessage('Computer wins!');
             setCounterMessage('');
-          }
-    }, [playerWins, computerWins])
+        }
+    }, [winner, playerChoice, computerChoice, playerWins, computerWins])
 
     return(
         <div className='results' role='complementary' >
-            <div data-testid='round-info' style={roundStyle} className={className} >{winner === null ? 'Choose rock, paper or scissors!' : roundMessage}</div>
-            <div data-testid='counter' style={counterStyle}>{winner === null ? '' : counterMessage}</div>
+            <div data-testid='round-info' className={className}>{winner === null ? 'Choose rock, paper or scissors!' : roundMessage}</div>
+            <div data-testid='counter' className={className}>{winner === null ? '' : counterMessage}</div>
         </div>
     )
 }
